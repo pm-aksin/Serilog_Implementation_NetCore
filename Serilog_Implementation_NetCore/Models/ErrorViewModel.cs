ce99f60465d6e851a6ba9bcad3ae32adc7c2efab
ASingh <Ablesh@DESKTOP-H9S2E76>
2024-12-29 21:35:25 +0530

    Add project files.

---
+++ Serilog_Implementation_NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Serilog_Implementation_NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
